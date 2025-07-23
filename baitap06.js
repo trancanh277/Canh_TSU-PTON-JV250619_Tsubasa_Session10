97b3f314052814996cc54d83df061fa11f0d8850
trancanh277 <tranminhcanh.tp@gmail.com>
2025-07-23 21:15:27 +0900

    upload session 10 bai tap 6

---
+++ baitap06.js
@@ -0,0 +1,5 @@
+let math = +prompt("Nhập vào điểm môn toán: ");
+let physics = +prompt("Nhập vào điểm môn vật lý: ");
+let chemistry = +prompt("Nhập vào điểm môn hóa: ");
+let average = (math + physics + chemistry) / 3;
+alert(`Điểm trung bình 3 môn học là: ` +average);
